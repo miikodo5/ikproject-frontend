22d439c3ed60a778fdb5050220b82b44e47e2f0b
miikodo5 <koliesnik.anton@gmail.com>
2024-04-13 16:05:57 +0300

    add link to Contact Us button

--- src/app/_components/app-button/index.tsx
+++ src/app/_components/app-button/index.tsx
@@ -1,10 +1,19 @@
 import React, {FC} from 'react';
+import Link from "next/link";
 
 type IButtonApp = {
     text: string,
     disabled?: boolean
 }
 const ButtonApp: FC<IButtonApp> = ({text, disabled}) => {
+    if(text === 'Contact Us'){
+        return (
+            <Link href={'#contact'} type={"submit"} className={'w-max text-sm px-8 py-3 rounded-[10px] transition duration-500 active:bg-mainbg' +
+                ' max-phone:text-sm ' + (disabled ? 'bg-grey-3 text-black cursor-default border border-grey-stroke' :  'text-white bg-main hover:shadow-[0_4px_16px_0_rgba(151,0,0,0.35)] cursor-pointer')}>
+                {text}
+            </Link>
+        );
+    }
     return (
         <button disabled={disabled} type={"submit"} className={'w-max text-sm px-8 py-3 rounded-[10px] transition duration-500 active:bg-mainbg' +
             ' max-phone:text-sm ' + (disabled ? 'bg-grey-3 text-black cursor-default border border-grey-stroke' :  'text-white bg-main hover:shadow-[0_4px_16px_0_rgba(151,0,0,0.35)] cursor-pointer')}>
